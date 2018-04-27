1d82329f231111fdbe5950abc63da87023c67e9c
Joske <joskekostic@gmail.com>
2018-04-27 09:34:32 +0200

    -This commit will add inital commit for react-routing

---
+++ react-routing/src/components/world.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class World extends Component {
+    render() {
+        return (
+            <div>World!</div>
+        )
+    }
+}
--- src/movie.js
+++
@@ -1,9 +0,0 @@
-export class Movie {
-
-    constructor(title,year,rating){
-        this.title = title;
-        this.year = year;
-        this.rating = rating;
-    }
-
-}
