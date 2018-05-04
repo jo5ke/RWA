f74d8fd98ca8d18b1308a6aa135f80d222ff6a14
Joske <joskekostic@gmail.com>
2018-05-04 09:43:24 +0200

    -This commit will install angular app, delete master branch files, create movie component

--- src/index2.js
+++
@@ -1,3 +0,0 @@
-import { MoviesService } from './another-movies.service';
-
-MoviesService.get().then(movies => console.log(movies));
--- src/random-number-service.js
+++
@@ -1,5 +0,0 @@
-export function getRandomNumber() {
-    return new Promise((resolve,reject)=>{
-        setTimeout( () => resolve(parseInt(Math.random()*10)),2000);
-    }); 
-}
